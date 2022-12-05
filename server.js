1e4bd16a15a998b358938d10fca392d7c019e067
Vishal <singhdhamivishal@gmail.com>
2022-12-05 16:50:20 +0530

    finalchanges

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static('build'));
+// app.use((req, res, next) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 const userSocketMap = {};
 function getAllConnectedClients(roomId) {
@@ -63,8 +63,6 @@ io.on('connection', (socket) => {
         socket.leave();
     });
 });
-app.get("/", (req, res) => {
-    res.send("Express on Vercel");
-  });
+
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
